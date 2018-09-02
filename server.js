05e0d0aa41c32959894966745c4b8fd781a5afce
Miguel Enrique Guerra Topete <guetop@MacBook-Pro-de-Miguel.local>
2018-09-02 13:08:22 -0500

    port fix

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
-var express = require("express");
-var path = require("path");
-var bodyParser = require("body-parser");
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
 var app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use("/scripts", express.static(__dirname + "/node_modules/"));
-app.use(express.static(path.join(__dirname, "./client")));
-app.use(express.static(__dirname + "./client"));
+app.use('/scripts', express.static(__dirname + '/node_modules/'));
+app.use(express.static(path.join(__dirname, './client')));
+app.use(express.static(__dirname + './client'));
 
-require("./server/config/mongoose.js");
+require('./server/config/mongoose.js');
 
-app.get("/*", function(req, res, next){
-	res.sendFile(__dirname + "/client/index.html");
+app.get('/*', function(req, res, next) {
+  res.sendFile(__dirname + '/client/index.html');
 });
 
-var routes = require("./server/config/routes.js");
+var routes = require('./server/config/routes.js');
 routes(app);
 
-app.listen(8000, function(){
-	console.log("Listening on port 8000");
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, function() {
+  console.log('Listening on port 8000');
 });
