28fdfb71f3179e8c2001772b031029e9f1c58e1c
Miguel Enrique Guerra Topete <guetop@MacBook-Pro-de-Miguel.local>
2018-09-03 17:17:22 -0500

    privacy policy

--- client/js/config.js
+++ client/js/config.js
@@ -1,41 +1,43 @@
 var myApp = angular.module("myApp", ["ngRoute", "ngMessages"]);
 
-myApp.config(function($routeProvider, $locationProvider){
-	$routeProvider
-	.when("/", {
-		templateUrl: "partials/home.html"
-	})
-	.when("/quote", {
-		templateUrl: "partials/quote.html"
-	})
-	.when("/about_us", {
-		templateUrl: "partials/about_us.html"
-	})
-	.when("/services", {
-		templateUrl: "partials/services.html"
-	})
-	.when("/contact", {
-		templateUrl: "partials/contact.html"
-	})
-	.when("/products", {
-		templateUrl: "partials/products.html"
-	})
-	.when("/distribution", {
-		templateUrl: "partials/servicios_de_distribucion.html"
-	})
-	.when("/farm_products", {
-		templateUrl: "partials/farm_products.html"
-	})
-	.when("/food_packaging", {
-		templateUrl: "partials/food_packaging.html"
-	})
-	.otherwise({
-		redirectTo: "/"
-	});
+myApp.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+    .when("/", {
+      templateUrl: "partials/home.html"
+    })
+    .when("/quote", {
+      templateUrl: "partials/quote.html"
+    })
+    .when("/about_us", {
+      templateUrl: "partials/about_us.html"
+    })
+    .when("/services", {
+      templateUrl: "partials/services.html"
+    })
+    .when("/contact", {
+      templateUrl: "partials/contact.html"
+    })
+    .when("/products", {
+      templateUrl: "partials/products.html"
+    })
+    .when("/distribution", {
+      templateUrl: "partials/servicios_de_distribucion.html"
+    })
+    .when("/farm_products", {
+      templateUrl: "partials/farm_products.html"
+    })
+    .when("/food_packaging", {
+      templateUrl: "partials/food_packaging.html"
+    })
+    .when("/privacy_policy", {
+      templateUrl: "partials/politica_privacidad.html"
+    })
+    .otherwise({
+      redirectTo: "/"
+    });
 
-	$locationProvider.html5Mode({
-		enabled: true,
-		requireBase: false
-	});
-
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  });
 });
