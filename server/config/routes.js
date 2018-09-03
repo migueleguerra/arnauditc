42906b2d99017359f6a4e966f2bec636063a55f7
Miguel Enrique Guerra Topete <guetop@MacBook-Pro-de-Miguel.local>
2018-09-02 22:49:21 -0500

    setInterval

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,12 +1,12 @@
-var cliente = require('./../controllers/clientes.js');
-var http = require('http');
+var cliente = require("./../controllers/clientes.js");
+var https = require("https");
 
 module.exports = function(app) {
-  app.post('/crearCliente', function(req, res) {
+  app.post("/crearCliente", function(req, res) {
     cliente.crearCliente(req, res);
   });
 
   setInterval(function() {
-    http.get('https://salty-cliffs-33321.herokuapp.com/');
+    https.get("https://salty-cliffs-33321.herokuapp.com/");
   }, 300000);
 };
