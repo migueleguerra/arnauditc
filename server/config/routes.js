23277ad64129d2764ebc8922d46c8bb2f5329adb
Miguel Enrique Guerra Topete <guetop@MacBook-Pro-de-Miguel.local>
2018-09-05 02:16:22 -0500

    remove interval

--- server/config/routes.js
+++ server/config/routes.js
@@ -5,8 +5,4 @@ module.exports = function(app) {
   app.post("/crearCliente", function(req, res) {
     cliente.crearCliente(req, res);
   });
-
-  setInterval(function() {
-    https.get("https://salty-cliffs-33321.herokuapp.com/");
-  }, 300000);
 };
